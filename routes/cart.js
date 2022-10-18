9fdc3da9eec7320abb897fbf838fc8c51507aefd
Eduardo Prieto <eduardo.prieto@nexgen-soluciones.com>
2022-10-18 08:14:44 -0500

    setting up the basic structure of endpoints

---
+++ routes/cart.js
@@ -0,0 +1,7 @@
+const router = require("express").Router();
+const controller = require("../controller/cart");
+//const auth = require("../middleware/auth");
+
+router.use("/", controller.getCurrentCart);
+
+module.exports = router;
