aa4c33228a83d6048cf8d44da4b6d119d9e33723
Eduardo Prieto <eduardo.prieto@nexgen-soluciones.com>
2022-10-18 23:55:56 -0500

    adding the authentication strategies for passport

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,7 @@
 const router = require("express").Router();
 
 router.use("/", require("./swagger"));
+router.use("/auth", require("./auth"));
 router.use("/cart", require("./cart"));
 
 module.exports = router;
