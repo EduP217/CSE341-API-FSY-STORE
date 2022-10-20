0b7ad8f3952741b641ab4cd4442c77215d4ebef6
Eduardo Prieto <eduardo.prieto@nexgen-soluciones.com>
2022-10-20 15:35:36 -0500

    update profile response

--- controller/profile.js
+++ controller/profile.js
@@ -7,12 +7,26 @@ const getProfile = async (req, res, next) => {
 
   await clientModel
     .findOne({ userId })
-    .then((data) => {
-      if (!data)
+    .then((client) => {
+      if (!client)
         return next(
           createError(404, `The client with UserId: ${userId} was not found`)
         );
-      return res.status(200).json(data);
+      
+      const p = {
+        name : req.user.name,
+        photoImage : req.user.photoImage,
+        roleID : req.user.roleID,
+        address: client.address,
+        phoneNumber: client.phoneNumber,
+        email: client.email,
+        birthday: client.birthday,
+        createdAt : req.user.createdAt,
+        OAuthProvider : req.user.provider
+      }
+      
+      console.log(p);
+      return res.status(200).json(p);
     })
     .catch((err) => next(createError(500, err)));
 };
@@ -31,7 +45,7 @@ const updateProfile = async (req, res, next) => {
             return next(
               createError(
                 404,
-                `Cannot update client profile with UserId: ${UserId}, maybe it was not found`
+                `Cannot update client profile with UserId: ${userId}, maybe it was not found`
               )
             );
           return res
